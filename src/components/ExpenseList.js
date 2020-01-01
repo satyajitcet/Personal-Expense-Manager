a9e60de01d170f380172ca641fc7e72a7bdb2573
Satyajit <satyajit1993bbsr@gmail.com>
2020-01-01 17:06:55 +0530

    Date formmating changes

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -2,13 +2,18 @@ import React from 'react';
 import {connect} from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
 import expenses from '../selectors/expenses';
-export const ExpenseList = (props) => (
+export const ExpenseList = (props) => {
+    let display_str = 'No Expense';
+    if(props.expenses.length>0){
+        console.log('length is more');
+        display_str = 'ExpenseList';}
+    return(
 <div>
-    <h1>ExpenseList</h1>
+    <h1>{display_str}</h1>
     {props.expenses.map((expense) =>(<ExpenseListItem key = {expense.id} {...expense}/>))}
     
 </div>
-);
+)};
 
 const mapStateToProps = (state) =>{
     return {
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,13 +2,18 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {removeExpense} from '../actions/expenses';
+import moment from 'moment';
+import numeral from 'numeral';
 export const ExpenseListItem = ({dispatch,id,description,amount,createdAt}) => {
     return(
 <div>
     <Link to = {`/edit/${id}`}>
 <h3>{description}</h3>
 </Link>
-<p>{amount} - {createdAt}</p>
+<p>
+{numeral(amount/100).format('$0,0.00')}
+ - 
+ {moment(createdAt).format('MMMM Do YYYY')}</p>
 <button onClick={(e) =>{dispatch(removeExpense({id}))}}>Remove</button>
 </div>
 )};
